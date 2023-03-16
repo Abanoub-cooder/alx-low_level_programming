d7fc0b2aa5f1cbd9ee426b70babcd2d2349d8b24
“Abanoub-cooder” <hoodegy@gmail.com>
2023-03-16 09:42:48 -0700

    tt

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ * main -this is main function
+ *
+ * Returm always 0
+ */
 int main(void)
 {
-	puts("/"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 		return (0);
 }
