244a0edf4e7d945f7ef753a8d89f3e66597139e5
Abanoub-cooder <hoodegy@gmail.com>
2023-04-03 07:25:04 -0700

    40

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -16,8 +16,8 @@ unsigned int _strspn(char *s, char *accept)
 		{
 			if (*s == accept[v])
 			{
-					n++;
-					break;
+				n++;
+				break;
 			}
 			else if (accept[v + 1] == '\0')
 				return (n);
