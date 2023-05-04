3e7205b25cdb589ff9ce6388ced1f65be21db48d
Abanoub-cooder <hoodegy@gmail.com>
2023-05-04 04:41:37 -0700

    betty

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * set_bit - it's sets a bit in a given index to 1 to us 
+ * set_bit - it's sets a bit in a given index to 1 to us
  * @n: paramenter pointer to the number to change
- * @index: it's index of the bit to set to 1 like first 
+ * @index: it's index of the bit to set to 1 like first
  *
  * Return: only 1 for success, -1 for failure
  */
@@ -10,9 +10,9 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 63)
 		return (-1);
-/*finish here */
+	/*finish here */
 
 	*n = ((1UL << index) | *n);
 	/*or finih here where -1mean stop*/
-    return (1);
+	return (1);
 }
